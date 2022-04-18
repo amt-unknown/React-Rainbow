83d66b7693a2340d9a87f701122a1240d2f9acf3
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-04-18 06:30:04 -0700

    assignment finished. Minor formatting and styling handled. Most bonuses implemented.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import ColorBLock from './ColorBlock'
+import ColorForm from './ColorForm'
 import './App.css';
 
 function App() {
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+
+  let colorMap = colors.map((color,i) => {
+    return(
+      <ColorBLock key={i} color={color} />
+    )
+  })
+
+  const checkColor = (newColor) =>  {
+    const c = new Option().style;
+    c.color = newColor
+    return c.color !== ""
+  }
+
+  const addColor = (newColor) => {
+    if(checkColor(newColor)){
+      setColors([...colors, newColor])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="colorCards">
+        {colorMap}  
+      </div>
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
